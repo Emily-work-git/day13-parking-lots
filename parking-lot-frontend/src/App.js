8a0f47a47d71d7b0821787bc7b4700dbd587b6cd
Emily Ng <ngem3@oocl.com>
2024-12-04 17:53:52 +0800

    init: initialize react project

---
+++ parking-lot-frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ParkingComponent from './components/ParkingComponent';
+
+function App() {
+  return (
+    <div className="App" style={{ margin: '20px' }}>
+      <ParkingComponent />
+    </div>
+  );
+}
+
+export default App;
